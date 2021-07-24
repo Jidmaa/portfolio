6bfe495c59c814f134b70fe3d32a7e1c5996f704
Jidmaa <majidbenhenneda@gmail.com>
2021-07-24 23:46:28 +0100

    feat: 🎸 done home page !

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,3 +1,7 @@
+import button from "../assets/button.svg";
+import robot from "../assets/robot.svg";
+import circle from "../assets/circle.svg";
+
 export default function Home() {
   return (
     <>
@@ -18,8 +22,58 @@ export default function Home() {
             <div className="lg:text-3xl text-xl "> 1.0 </div>
           </div>
         </div>
-        <div className=" xl:w-2/5 w-screen text-text flex  justify-evenly w flex-row items-center border-b border-r border-primary">
-          cc
+        <div className="flex-col py-8 px-14 xl:w-2/5 w-screen text-text flex justify-between flex-row  border-b border-r border-primary">
+          <h1 className="text-5xl  ">
+            I make good looking websites that give that
+            <span className="text-primary font-bold"> WOAH </span> factor.
+          </h1>
+          <div>
+            <div className="flex flex-row text-4xl font-extrabold italic justify-between uppercase">
+              <span> excellence </span> <span>is </span>
+            </div>
+            <div className="py-2">
+              Let's read between the lines shall we ? if you didn't get my name
+              yet, I'm Majid Benhenneda.
+            </div>
+            <span className="text-4xl font-extrabold italic  uppercase">A</span>
+            <div className="px-8 text-4xl font-extrabold italic  uppercase">
+              HABIT
+            </div>
+          </div>
+          <div>
+            I am a software engineer and a front-end developer, always striving
+            to get the most out of the projects I work on, I love collaborating
+            on exciting works and contribute{" "}
+          </div>
+          <div className="ml-auto px-8 flex flex-row items-center">
+            Check out my projects !{" "}
+            <img className="px-8" src={button} alt="button" />
+          </div>
+          <div className="flex justify-between">
+            <div className="flex  w-2/5 flex-row">
+              <div className="text-6xl font-extrabold text-primary pr-6">
+                {" "}
+                3{" "}
+              </div>
+              <div> Years of thambir in thambir land </div>
+            </div>
+            <div className="flex w-2/5 flex-row">
+              <div className="text-6xl font-extrabold text-primary pr-6">
+                {" "}
+                8{" "}
+              </div>
+              <div> Projects in productions </div>
+            </div>
+            <div className="flex w-2/5 flex-row">
+              <div className="text-6xl  font-extrabold text-primary pr-6">
+                4
+              </div>
+              <div> Bruh of bruh to the burh bruh </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-text flex items-center jusitfy-center ">
+          <img src={robot} />
         </div>
       </div>
     </>
